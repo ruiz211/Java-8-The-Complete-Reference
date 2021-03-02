0362cb51157047b8d72430ba69be64bdac479c0d
ruiz211 <ruiz.im211@gmail.com>
2021-03-01 19:39:11 -0500

    Abstract classes

--- Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/VIII/Inheritance/AbstractClass.java
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/VIII/Inheritance/AbstractClass.java
@@ -1,7 +1,7 @@
 package com.ruiz211.TheJavaLanguage.VIII.Inheritance;
 
 public class AbstractClass {
-    public static void main(String[] args) {
+    public static void main(String... doYourBest) {
         Rectangle r = new Rectangle(10, 10);
         System.out.println(r.getArea());
 

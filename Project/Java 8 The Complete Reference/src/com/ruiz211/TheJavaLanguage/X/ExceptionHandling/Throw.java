9023827ad86b320b25e90b44f63bfe5366a9699a
ruiz211 <ruiz.im211@gmail.com>
2021-03-02 16:46:58 -0500

    Throw

--- Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/X/ExceptionHandling/Throw.java
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/X/ExceptionHandling/Throw.java
@@ -2,14 +2,15 @@ package com.ruiz211.TheJavaLanguage.X.ExceptionHandling;
 
 public class Throw {
     public static void main(String... doYourBest) {
-        validate(13);
+        validate(1);
     }
 
-    static void validate(int age) {
-        if (age < 18)
-            throw new ArithmeticException("Age not valid");
-        else {
+    private static void validate(int age) {
+        try {
+            if (age < 18) throw new ArithmeticException("Age not valid");
             System.out.println("Welcome");
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
         }
     }
 }

edeb7fd0c913dba944d1b848257bfcf2531a9229
ruiz211 <ruiz.im211@gmail.com>
2021-03-01 19:12:20 -0500

    Packages

--- Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/VIIII/PackagesAndInterfaces/Packages.java
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/VIIII/PackagesAndInterfaces/Packages.java
@@ -3,7 +3,7 @@ package com.ruiz211.TheJavaLanguage.VIIII.PackagesAndInterfaces;
 import com.ruiz211.TheJavaLanguage.VIIII.PackagesAndInterfaces.one.OneOne;
 import com.ruiz211.TheJavaLanguage.VIIII.PackagesAndInterfaces.one.two.Two;
 
-public class Root {
+public class Packages {
     public static void main(String[] args) {
         System.out.println("Message from OneOne class" + new OneOne().toString());
         System.out.println("Message from Two class" + new Two().toString());

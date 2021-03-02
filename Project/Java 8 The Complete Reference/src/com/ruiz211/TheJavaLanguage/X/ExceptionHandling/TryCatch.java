4d72f4a13d56a8d022715b014d0ca484f628422f
ruiz211 <ruiz.im211@gmail.com>
2021-03-01 20:26:25 -0500

    Multiple Try catch

--- Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/X/ExceptionHandling/TryCatch.java
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/X/ExceptionHandling/TryCatch.java
@@ -3,6 +3,7 @@ package com.ruiz211.TheJavaLanguage.X.ExceptionHandling;
 public class TryCatch {
     public static void main(String... doYourBest) {
         System.out.println("ArithmeticException " + ArithmeticExceptions.division());
+        System.out.println("ArrayIndexOutOfBoundsException " + MultipleCatches.doubleCatches());
     }
 }
 
@@ -21,3 +22,18 @@ class ArithmeticExceptions {
     }
 }
 
+class MultipleCatches {
+    static int a = 11;
+    static byte b = 0;
+    static int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    static int doubleCatches() {
+        int c = 0;
+        try {
+            c = a / array[10];
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException ae) {
+            System.out.println("Message: " + ae.getMessage());
+        }
+        return c;
+    }
+}

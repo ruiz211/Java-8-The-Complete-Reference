6f0b8f596bac8f3f526112e18ed13375ab342f64
ruiz211 <ruiz.im211@gmail.com>
2021-03-01 19:42:08 -0500

    Overloading methods

--- Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/VII/ACloserLookAtMethodsAndClasses/OverloadingMethods.java
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/VII/ACloserLookAtMethodsAndClasses/OverloadingMethods.java
@@ -1,7 +1,7 @@
 package com.ruiz211.TheJavaLanguage.VII.ACloserLookAtMethodsAndClasses;
 
 public class OverloadingMethods {
-    public static void main(String[] args) {
+    public static void main(String... doYourBest) {
 
     }
 

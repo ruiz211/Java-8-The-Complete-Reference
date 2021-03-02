1c1885690c06afbf74b06113696bd7fae2ba3e22
ruiz211 <ruiz.im211@gmail.com>
2021-03-01 19:37:52 -0500

    If statement

--- Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/V/ControlStatements/Ifs.java
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/V/ControlStatements/Ifs.java
@@ -1,7 +1,7 @@
 package com.ruiz211.TheJavaLanguage.V.ControlStatements;
 
 public class Ifs {
-    public static void main(String[] args) {
+    public static void main(String... doYourBest) {
         int a = 10;
         int b = 11;
 

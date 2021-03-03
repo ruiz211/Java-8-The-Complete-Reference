dfbe824ec255b4dc2c190905f94ad6d978e93c6a
ruiz211 <ruiz.im211@gmail.com>
2021-03-02 22:34:15 -0500

    Functional interfaces

--- Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/XV/LambdaExpressions/Lambdas.java
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/XV/LambdaExpressions/Lambdas.java
@@ -11,5 +11,17 @@ public class Lambdas {
 
         Compare c = Object::equals;
         System.out.println(c.test(12, 10));
+
+        Factorial f = factor -> {
+            int result = 1;
+            if (factor < 0) return 0;
+            else if (factor == 0) return 1;
+            for (int i = 1; i <= factor; i++) {
+                result *= i;
+            }
+            return result;
+        };
+
+        System.out.println(f.getFacto(5));
     }
 }

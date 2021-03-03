cc02b20b817da3425588c567f03c78e3188d298a
ruiz211 <ruiz.im211@gmail.com>
2021-03-02 22:34:39 -0500

    Autoboxing

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/XII/EnumerationsAutoboxingAndAnnotations/Autoboxing.java
@@ -0,0 +1,10 @@
+package com.ruiz211.TheJavaLanguage.XII.EnumerationsAutoboxingAndAnnotations;
+
+public class Autoboxing {
+    public static void main(String... doYourBest) {
+        int i = 20;
+        Integer ii = 11;
+        System.out.println(ii.compareTo(i));
+
+    }
+}

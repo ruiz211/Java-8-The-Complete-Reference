d8a8503978c894c062d6932c5302f824179a1eae
ruiz211 <ruiz.im211@gmail.com>
2021-03-03 19:15:15 -0500

    Doubles

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLibrary/II/ExploringJavaLang/Doubles.java
@@ -0,0 +1,19 @@
+//
+//  Doubles.java
+//  Java SE 8 The Complete Reference
+//
+//  Created by José Ruiz on 1/2/20.
+//  Copyright © 2020 José Ruiz. All rights reserved.
+//
+
+//
+
+package com.ruiz211.TheJavaLibrary.II.ExploringJavaLang;
+
+public class Doubles {
+    public static void main(String... doYourBest) {
+        System.out.println("Double.MAX_VALUE: " + Double.MAX_VALUE);
+        System.out.println("Double.MIN_VALUE: " + Double.MIN_VALUE);
+        System.out.println("Double.sum(Double.MAX_VALUE + Double.MIN_VALUE): " + Double.sum(Double.MAX_VALUE, Double.MIN_VALUE));
+    }
+}

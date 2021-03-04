6201ef180949889fd5063c4408f23bd0ae85c6d3
ruiz211 <ruiz.im211@gmail.com>
2021-03-03 19:15:41 -0500

    Lambda as arguments

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/XV/LambdaExpressions/LambdaAsArgument.java
@@ -0,0 +1,20 @@
+package com.ruiz211.TheJavaLanguage.XV.LambdaExpressions;
+
+interface ArgumentInterface {
+    String func(String ai);
+}
+
+public class LambdaAsArgument {
+    public static void main(String... doYourBest) {
+        System.out.println(Argument.stringOperation(ai -> ai.toUpperCase(), "Some new string"));
+        System.out.println(Argument.stringOperation(String::toLowerCase, "Some new string"));
+    }
+}
+
+class Argument {
+
+    static String stringOperation(ArgumentInterface as, String ai) {
+        return as.func(ai);
+    }
+
+}

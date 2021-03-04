d57c0cbcec843d9b316dd153078cdc36691db167
ruiz211 <ruiz.im211@gmail.com>
2021-03-03 19:15:48 -0500

    Longs

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLibrary/II/ExploringJavaLang/Longs.java
@@ -0,0 +1,17 @@
+//
+//  Longs.java
+//  Java SE 8 The Complete Reference
+//
+//  Created by José Ruiz on 1/2/20.
+//  Copyright © 2020 José Ruiz. All rights reserved.
+//
+
+//
+
+package com.ruiz211.TheJavaLibrary.II.ExploringJavaLang;
+
+public class Longs {
+    public static void main(String... doYourBest) {
+
+    }
+}

b371460ffe3dd3924adf4b8c7d4bcfe9b81d6bd7
ruiz211 <ruiz.im211@gmail.com>
2021-03-09 21:32:48 -0500

    Introducing Nested Classes

--- Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/VII/ACloserLookAtMethodsAndClasses/IntroducingNestedClasses.java
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLanguage/VII/ACloserLookAtMethodsAndClasses/IntroducingNestedClasses.java
@@ -1,7 +1,7 @@
 package com.ruiz211.TheJavaLanguage.VII.ACloserLookAtMethodsAndClasses;
 
 public class IntroducingNestedClasses {
-    public static void main(String[] args) {
+    public static void main(String... doYourBest) {
 
     }
 }

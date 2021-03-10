465e0760b90aacd3b88d3a5f1c91e2fff73776d9
ruiz211 <ruiz.im211@gmail.com>
2021-03-09 21:33:23 -0500

    Strings Searching

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLibrary/I/StringHandling/StringsSearching.java
@@ -0,0 +1,13 @@
+package com.ruiz211.TheJavaLibrary.I.StringHandling;
+
+public class StringsSearching {
+    public static void main(String... doYourBest) {
+        String name = "John";
+
+        //indexOf()
+        System.out.println(name.indexOf('n'));
+
+        //lastIndexOf()
+        System.out.println(name.lastIndexOf('o'));
+    }
+}

f8b622696c8712a82f8cb731c2103bcd82e5a1bc
ruiz211 <ruiz.im211@gmail.com>
2021-03-10 20:58:13 -0500

    Explicit Channel Read

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLibrary/VI/NIO/ExplicitChannelRead.java
@@ -0,0 +1,19 @@
+//
+//  ExplicitChannelRead.java
+//  Java SE 8 The Complete Reference
+//
+//  Created by José Ruiz on 3/20/20.
+//  Copyright © 2020 José Ruiz. All rights reserved.
+//
+
+//
+
+package com.ruiz211.TheJavaLibrary.VI.NIO;
+
+final class ExplicitChannelRead {
+    ExplicitChannelRead() {
+    }
+
+    public static void main(String... doYourBest) {
+    }
+}

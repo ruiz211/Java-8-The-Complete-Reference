af2548c8d431eeaa3bceef020b657779012cb092
ruiz211 <ruiz.im211@gmail.com>
2021-03-12 17:46:58 -0500

    Boolean

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/iii/Booleans.java
@@ -0,0 +1,10 @@
+package com.ruiz211.i.languaje.iii;
+
+public class Booleans {
+
+    public static void main(String... doYourBest) {
+        boolean isBool = true;
+        boolean isBoolTwo = false;
+        if (isBool != isBoolTwo) System.out.println("They are different");
+    }
+}

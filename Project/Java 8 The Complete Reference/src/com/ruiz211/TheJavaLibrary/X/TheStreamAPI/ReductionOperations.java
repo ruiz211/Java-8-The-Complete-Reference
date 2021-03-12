b9bd698e0f6a83098a2539741d8aee086a2c6c11
ruiz211 <ruiz.im211@gmail.com>
2021-03-11 20:10:16 -0500

    Reduction Operation

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/TheJavaLibrary/X/TheStreamAPI/ReductionOperations.java
@@ -0,0 +1,24 @@
+//
+//  ReductionOperations.java
+//  Java SE 8 The Complete Reference
+//
+//  Created by José Ruiz on 1/31/20.
+//  Copyright © 2020 José Ruiz. All rights reserved.
+//
+
+//
+
+package com.ruiz211.TheJavaLibrary.X.TheStreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+final class ReductionOperations {
+    ReductionOperations() {
+    }
+
+    public static void main(String... doYourBest) {
+        List<Integer> list = Arrays.asList(1, 23, 54, 34, 87, 90, 21, 34, 5, 6, 9, 34, 6, 12);
+        System.out.println(list.stream().reduce((x, y) -> x * y));
+    }
+}

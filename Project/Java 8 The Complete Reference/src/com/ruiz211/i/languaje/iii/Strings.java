5108edbc85b8a57005522b46715d9b9d9dfe9f90
ruiz211 <ruiz.im211@gmail.com>
2021-03-12 17:52:30 -0500

    String

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/iii/Strings.java
@@ -0,0 +1,9 @@
+package com.ruiz211.i.languaje.iii;
+
+public class Strings {
+
+    public static void main(String... doYourBest) {
+        String string = "Hello world";
+        System.out.println("\"" + string + "\"");
+    }
+}

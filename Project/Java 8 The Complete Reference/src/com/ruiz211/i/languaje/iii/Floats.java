42c9286520c9216d060c7917dcc29594203a8b91
ruiz211 <ruiz.im211@gmail.com>
2021-03-12 17:50:09 -0500

    Float

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/iii/Floats.java
@@ -0,0 +1,9 @@
+package com.ruiz211.i.languaje.iii;
+
+public class Floats {
+
+    public static void main(String... doYourBest) {
+        float floatNumber = 5.0F; //float can be useful when representing dollars and cents or temperture
+        System.out.println(floatNumber);
+    }
+}

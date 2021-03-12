3531516b62e5c3f51c6a95e8eebd845b28ab745e
ruiz211 <ruiz.im211@gmail.com>
2021-03-12 17:48:34 -0500

    Escape Sequences

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/iii/EscapeSequences.java
@@ -0,0 +1,30 @@
+//
+//  EscapeSequences.java
+//  Java SE 8 The Complete Reference
+//
+//  Created by José Ruiz on 9/28/19.
+//  Copyright © 2019 José Ruiz. All rights reserved.
+//
+
+package com.ruiz211.i.languaje.iii;
+
+public class EscapeSequences {
+
+    public static void main(String... doYourBest) {
+        /*
+
+        \'
+        \"
+        \\
+        \r
+        \n
+        \f
+        \t
+        \b
+        \ddd
+        \\uxxxx
+
+         */
+
+    }
+}

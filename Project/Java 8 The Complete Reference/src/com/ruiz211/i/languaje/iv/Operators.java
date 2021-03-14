470b528becd64a76dc1dcc39c9fa471ebe12b16e
ruiz211 <ruiz.im211@gmail.com>
2021-03-13 19:39:17 -0500

    Operators

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/iv/Operators.java
@@ -0,0 +1,51 @@
+package com.ruiz211.TheJavaLanguage.IV.Operators;
+
+public class Operators {
+    public static void main(String... doYourBest) {
+        /*
+
+
+
+
+        Bitwise Operators
+
+
+
+
+
+
+        Relational Operators
+
+        ==
+        !=
+        <
+        >
+        <=
+        >=
+
+
+        Logic Operators
+
+        &
+        |
+        ^
+        !
+        &&
+        ||
+
+
+        Abbreviations
+
+        +=
+        -=
+        *=
+        /=
+        %=
+        &=
+        |=
+        ^=
+
+
+        */
+    }
+}

b32f143745ffe0395596445c5c0ac55fd4be6634
ruiz211 <ruiz.im211@gmail.com>
2021-03-13 19:37:32 -0500

    Introducing Final

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/vii/IntroducingFinals.java
@@ -0,0 +1,14 @@
+package com.ruiz211.TheJavaLanguage.VII.ACloserLookAtMethodsAndClasses;
+
+import java.util.UUID;
+
+public class IntroducingFinals {
+
+    private static final String id = UUID.randomUUID().toString();
+
+    public static void main(String... doYourBest) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(id);
+        }
+    }
+}

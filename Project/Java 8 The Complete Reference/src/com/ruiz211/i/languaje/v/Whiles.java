cbc5c6daa5ca9f7db4434a154c1b5e37636d37cc
ruiz211 <ruiz.im211@gmail.com>
2021-03-13 19:40:40 -0500

    While loop

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/v/Whiles.java
@@ -0,0 +1,14 @@
+//
+//  Whiles.java
+//  Java 8 The Complete Reference
+//
+//  Created by José Ruiz on 3/13/21.
+//
+
+package com.ruiz211.i.languaje.v;
+
+public class Whiles {
+    public static void main(String... doYourBest) {
+        //
+    }
+}

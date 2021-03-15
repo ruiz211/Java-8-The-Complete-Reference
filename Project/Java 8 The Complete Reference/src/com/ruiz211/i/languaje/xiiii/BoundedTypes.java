917f371ea87fcc0c880ae884024e2a0216617783
ruiz211 <ruiz.im211@gmail.com>
2021-03-15 15:18:22 -0500

    Bounded Types

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/xiiii/BoundedTypes.java
@@ -0,0 +1,33 @@
+package com.ruiz211.i.languaje.xiiii;
+
+public class BoundedTypes {
+    public static void main(String... doYourBest) {
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Average<Integer> average = new Average<>(array);
+        System.out.println(average.average());
+    }
+}
+
+class Average<T extends Number> {
+    T[] t;
+
+    Average(T[] t) {
+        this.t = t;
+    }
+
+    private T[] getT() {
+        return t;
+    }
+
+    private T getT(int i) {
+        return t[i];
+    }
+
+    double average() {
+        double sum = 0;
+        for (int i = 0; i < getT().length; i++) {
+            sum += getT(i).doubleValue();
+        }
+        return sum / t.length;
+    }
+}

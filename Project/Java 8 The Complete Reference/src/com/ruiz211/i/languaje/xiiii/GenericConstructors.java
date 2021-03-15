bf25225897a6e1815973b2a8199f8b92a5b2017d
ruiz211 <ruiz.im211@gmail.com>
2021-03-15 15:20:02 -0500

    Generic Constructor

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/xiiii/GenericConstructors.java
@@ -0,0 +1,35 @@
+package com.ruiz211.i.languaje.xiiii;
+
+public class GenericConstructors {
+    public static void main(String... doYourBest) {
+        Constructors a = new Constructors(12.3);
+        Constructors b = new Constructors(37.0F);
+
+        System.out.println(a.toString());
+        System.out.println(b.toString());
+
+    }
+}
+
+class Constructors {
+    private double value;
+
+    <T extends Number> Constructors(T value) {
+        this.value = value.doubleValue();
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Constructors{" +
+                "value=" + getValue() +
+                '}';
+    }
+}

2a8cbeccfad9484ba12a019bbbb9eeff89d1060a
ruiz211 <ruiz.im211@gmail.com>
2021-03-15 15:20:18 -0500

    Generic Hierarchies

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/xiiii/GenericHierarchies.java
@@ -0,0 +1,26 @@
+package com.ruiz211.i.languaje.xiiii;
+
+public class GenericHierarchies {
+    public static void main(String... doYourBest) {
+        HierarchiesII<String> ii = new HierarchiesII<>("Some string");
+        System.out.println(ii.getT());
+    }
+}
+
+class Hierarchies<T> {
+    protected T t;
+
+    Hierarchies(T t) {
+        this.t = t;
+    }
+
+    T getT() {
+        return t;
+    }
+}
+
+class HierarchiesII<T> extends Hierarchies<T> {
+    HierarchiesII(T t) {
+        super(t);
+    }
+}

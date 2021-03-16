51b3ee24261a36baf7e1ce3e89892c787a088c09
ruiz211 <ruiz.im211@gmail.com>
2021-03-16 15:26:49 -0500

    Wildcards

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/xiiii/Wildcards.java
@@ -0,0 +1,41 @@
+package com.ruiz211.i.languaje.xiiii;
+
+public class Wildcards {
+    public static void main(String... doYourBest) {
+        Integer[] arrayInteger = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Double[] arrayDouble = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9};
+        SameAverage<Integer> averageI = new SameAverage<>(arrayInteger);
+        SameAverage<Double> averageD = new SameAverage<>(arrayDouble);
+//        System.out.println(average.average());
+
+        System.out.println(averageI.compare(averageD));
+    }
+}
+
+class SameAverage<T extends Number> {
+    private T[] t;
+
+    SameAverage(T[] t) {
+        this.t = t;
+    }
+
+    private T[] getT() {
+        return t;
+    }
+
+    private T getT(int i) {
+        return t[i];
+    }
+
+    private double average() {
+        double sum = 0;
+        for (int i = 0; i < getT().length; i++) {
+            sum += getT(i).doubleValue();
+        }
+        return sum / t.length;
+    }
+
+    boolean compare(SameAverage<?> object) {
+        return average() == object.average();
+    }
+}

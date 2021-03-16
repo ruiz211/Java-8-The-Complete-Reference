149b2b85d1750131626464a45b2ace0c528fae39
ruiz211 <ruiz.im211@gmail.com>
2021-03-16 15:23:57 -0500

    Switch Statement

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/v/SwitchStatement.java
@@ -0,0 +1,14 @@
+//
+//  SwitchStatement.java
+//  Java 8 The Complete Reference
+//
+//  Created by José Ruiz on 3/10/21.
+//
+
+package com.ruiz211.i.languaje.v;
+
+public class SwitchStatement {
+    public static void main(String... doYourBest) {
+        //
+    }
+}

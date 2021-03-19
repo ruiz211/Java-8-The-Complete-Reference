35c84a707572a16899e5317fe6d88bb8b11801cd
ruiz211 <ruiz.im211@gmail.com>
2021-03-19 15:11:54 -0500

    Generic Interface

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/xiiii/MinMaxMain.java
@@ -0,0 +1,38 @@
+package com.ruiz211.i.languaje.xiiii;
+
+interface MinMaxInterface<T extends Comparable<T>> {
+    T min();
+
+    T max();
+}
+
+public class MinMaxMain {
+    public static void main(String... doYourBest) {
+        Integer[] i = {43, 56, 11, 78, 31, 55, 98};
+        MinMax<Integer> minMax = new MinMax<>(i);
+        System.out.println(minMax.min());
+        System.out.println(minMax.max());
+    }
+}
+
+class MinMax<T extends Comparable<T>> implements MinMaxInterface {
+    T[] t;
+
+    MinMax(T[] t) {
+        this.t = t;
+    }
+
+    @Override
+    public Comparable min() {
+        T u = t[0];
+        for (T aT : t) if (aT.compareTo(u) < 0) u = aT;
+        return u;
+    }
+
+    @Override
+    public Comparable max() {
+        T u = t[0];
+        for (T aT : t) if (aT.compareTo(u) > 0) u = aT;
+        return u;
+    }
+}

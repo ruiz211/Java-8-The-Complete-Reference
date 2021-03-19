cbc1859e1e0aaf0a17cd3decf44c1a491d6b1cad
ruiz211 <ruiz.im211@gmail.com>
2021-03-18 21:35:48 -0500

    Operators

--- Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/iv/Operators.java
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/iv/Operators.java
@@ -1,4 +1,11 @@
-package com.ruiz211.TheJavaLanguage.IV.Operators;
+//
+//  Operators.java
+//  Java 8 The Complete Reference
+//
+//  Created by José Ruiz on 3/10/21.
+//
+
+package com.ruiz211.i.languaje.iv;
 
 public class Operators {
     public static void main(String... doYourBest) {

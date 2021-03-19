953cbd55c88915248e53782f908cbf6c2ae7726d
ruiz211 <ruiz.im211@gmail.com>
2021-03-19 15:09:44 -0500

    Enum

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/xii/Chess.java
@@ -0,0 +1,18 @@
+package com.ruiz211.i.languaje.xii;
+
+public enum Chess {
+
+    King(1), Queen(1), Bishop(2), Knight(2), Rock(2), Pawn(8);
+
+    private int number;
+
+    Chess(int i) {
+        this.number = i;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
+
+

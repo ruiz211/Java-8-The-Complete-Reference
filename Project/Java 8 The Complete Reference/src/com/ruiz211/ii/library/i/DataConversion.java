e47a1c5e60dca978d3648e10a786f25325a9df69
ruiz211 <ruiz.im211@gmail.com>
2021-03-18 21:29:54 -0500

    Data Conversion

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/ii/library/i/DataConversion.java
@@ -0,0 +1,9 @@
+package com.ruiz211.ii.library.i;
+
+public class DataConversion {
+    public static void main(String... doYourBest) {
+        int age = 37;
+        String stringAge = String.valueOf(age);
+        System.out.println(stringAge);
+    }
+}

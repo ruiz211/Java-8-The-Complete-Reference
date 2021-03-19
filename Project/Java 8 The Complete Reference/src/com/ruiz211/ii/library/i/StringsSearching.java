4366814261f138e2e1597c7be8d4cdc18c27fe33
ruiz211 <ruiz.im211@gmail.com>
2021-03-18 21:33:26 -0500

    Strings searching

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/ii/library/i/StringsSearching.java
@@ -0,0 +1,13 @@
+package com.ruiz211.ii.library.i;
+
+public class StringsSearching {
+    public static void main(String... doYourBest) {
+        String name = "John";
+
+        //indexOf()
+        System.out.println(name.indexOf('n'));
+
+        //lastIndexOf()
+        System.out.println(name.lastIndexOf('o'));
+    }
+}

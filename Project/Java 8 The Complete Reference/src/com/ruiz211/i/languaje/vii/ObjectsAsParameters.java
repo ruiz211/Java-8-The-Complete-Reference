afd7db6047372ff56dd0bdec6bae057cd84ca3f8
ruiz211 <ruiz.im211@gmail.com>
2021-03-18 21:36:35 -0500

    Objects as parameters

--- Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/vii/ObjectsAsParameters.java
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/i/languaje/vii/ObjectsAsParameters.java
@@ -1,4 +1,4 @@
-package com.ruiz211.TheJavaLanguage.VII.ACloserLookAtMethodsAndClasses;
+package com.ruiz211.i.languaje.vii;
 
 public class ObjectsAsParameters {
     public static void main(String... doYourBest) {

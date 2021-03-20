3d39f904d5fa054f0c68baf95f1e017b44c9ee85
ruiz211 <ruiz.im211@gmail.com>
2021-03-20 16:47:21 -0500

    Map interface

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/ii/library/iii/Maps.java
@@ -0,0 +1,15 @@
+package com.ruiz211.ii.library.iii;
+
+import java.util.HashMap;
+import java.util.Map;
+
+final class Maps {
+    Maps() {
+    }
+
+    public static void main(String... doYourBest) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("1", 1);
+
+    }
+}

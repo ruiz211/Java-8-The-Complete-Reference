45b8a12652bbd5d16ef118ab8718e407aeb3057d
ruiz211 <ruiz.im211@gmail.com>
2021-03-20 16:55:59 -0500

    java.util: ArrayList class Usage example

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/ii/library/iii/ArrayLists.java
@@ -0,0 +1,17 @@
+//
+//  ArrayLists.java
+//  Java 8 The Complete Reference
+//
+//  Created by José Ruiz on 3/20/21.
+//
+
+package com.ruiz211.ii.library.iii;
+
+import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void main(String... doYourBest) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("");
+    }
+}

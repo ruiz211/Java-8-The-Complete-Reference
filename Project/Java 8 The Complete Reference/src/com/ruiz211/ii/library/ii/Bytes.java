4eea6e6d87acb8766380d3b6cdf5501a892874fc
ruiz211 <ruiz.im211@gmail.com>
2021-03-20 16:45:20 -0500

    Byte

---
+++ Project/Java 8 The Complete Reference/src/com/ruiz211/ii/library/ii/Bytes.java
@@ -0,0 +1,15 @@
+package com.ruiz211.ii.library.ii;
+
+public class Bytes {
+    public static void main(String... doYourBest) {
+        System.out.println("Byte.MAX_VALUE: " + Byte.MAX_VALUE);
+        System.out.println("Byte.MIN_VALUE: " + Byte.MIN_VALUE);
+        System.out.println("Byte.SIZE: " + Byte.SIZE);
+        System.out.println("Byte.TYPE: " + Byte.TYPE);
+
+        byte b = 100;
+        byte c = Byte.parseByte("100");
+
+        System.out.println("b + c: " + (b + c));
+    }
+}
